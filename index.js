e1a5c4b63faefbcd097058349690f214d84794ca
timmobrien <timmobrien@icloud.com>
2022-07-18 15:09:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+// Goal : generate readme file for future projects
+
+const chalk = require('chalk')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path =  require('path')
+const templateLocation = path.join(__dirname,'utils','template','readme-template.md')
+const outputLocation = path.join(__dirname, 'output', 'README.md')
+
+const greenConsole = chalk.green
+const blueConsole = chalk.blue
+
+console.log(greenConsole('Welcome to the README Generator! Please answer the following questions and your README file will be generated'))
+console.log(blueConsole('If you need to add code snippets or line breaks, please use backticks and <br> respectively'))
+
+// Questions to ask
+
+inquirer.prompt([
+    // what is the project title
+    {
+        type: 'input',
+        message: 'What is the project title?',
+        name:'title',
+    },
+    // Description
+    {
+        type: 'input',
+        message: 'What is the project description',
+        name:'description',
+    },
+    // installation instructions
+    {
+        type: 'input',
+        message: 'What are the project installation instructions?',
+        name:'installation',
+    },
+    // Usage information
+    {
+        type: 'input',
+        message: 'What is the project usage information?',
+        name:'usage',
+    },
+    // Contribution guidelines
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name:'contribution',
+    },
+    // test instructions
+    {
+        type: 'input',
+        message: 'What are the project test instructions?',
+        name:'test',
+    },
+    // email
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name:'email',
+    },
+    // license
+    {
+        type: 'list',
+        message: 'What is the license?',
+        name:'license',
+        choices: [
+            'MIT','GPL-3.0', 'Apache-v2', 'ISC'
+        ]
+    },
+    // github
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name:'github',
+    },
+    {
+        type:'input',
+        message:'What is the GitHub project URL?',
+        name: 'url'
+    }
+
+]).then((ans) => {
+    const lowerCaseLicense = ans.license.toLowerCase()
+    const template = fs.readFileSync(templateLocation, 'utf-8')
+    const licenses = /{{license}}/g;
+    const projectURL = /{{url}}/g
+    const profileLink = /{{github-link}}/g
+    const output = template.replace('{{title}}', ans.title)
+        .replace('{{licenseLowerCase}}', lowerCaseLicense)
+        .replace(licenses, ans.license)
+        .replace('{{description}}', ans.description)
+        .replace(projectURL, ans.url)
+        .replace('{{github}}', ans.github)
+        .replace('{{installation}}', ans.installation)
+        .replace('{{usage}}', ans.usage)
+        .replace('{{contribution}}',ans.contribution)
+        .replace('{{test}}',ans.test)
+        .replace(profileLink,`https://github.com/${ans.github}`)
+        .replace('{{email}}',ans.email)
+    
+    fs.writeFileSync(outputLocation, output)
+})
+
+
+
+// once questions are asked
+
+// generate readme file based on the responses
+
---
+++ utils/template/readme-template.md
@@ -0,0 +1,58 @@
+
+
+<center>
+
+# {{title}} <a name="title"></a><br>
+
+![mit-license](https://img.shields.io/badge/license-{{license}}-green)
+</center>
+
+## Description <a name="description"></a>
+
+{{description}}
+
+GitHub URL: [{{url}}]({{url}})
+
+## Table of Contents
+1. [Title](#title)
+
+2. [Description](#description)
+
+3. [Installation](#installation)
+
+4. [Usage](#usage)
+
+5. [License](#license)
+
+6. [Tests](#tests)
+
+7. [Questions](#questions)
+
+## Installation <a name="installation"></a>
+{{installation}} 
+
+## Usage <a name="usage"></a>
+{{usage}}
+
+## License <a name="license"></a>
+[{{license}}](https://choosealicense.com/licenses/{{licenseLowerCase}})
+
+Copyright © 2022 [@{{github}}]({{github-link}})
+
+## Contributing <a name="contributing"></a>
+{{contribution}}
+
+## Tests <a name="tests"></a>
+{{test}}
+
+## Questions <a name="questions"></a>
+
+If you have any questions, get in contact with me:
+
+[Github]({{github-link}})
+
+[E-mail]({{email}}) 
+
+__________________________________________________
+
+> *This README was generated using the [README Generator!](https://github.com/timmobrien/Node-README-Generator)*
